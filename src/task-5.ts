b02d6d9c41e01b78523a45cfa7524a07252d7ec9
user <mihailo1013@gmail.com>
2025-08-31 15:59:23 +0300

    commit-1

---
+++ src/task-5.ts
@@ -0,0 +1,12 @@
+type statusType ="loading" | "success" | "error"
+function logStatus(status:statusType):void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
