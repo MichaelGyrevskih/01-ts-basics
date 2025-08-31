e9624dad2d998c2c532539c1f0994f81c8493c43
user <mihailo1013@gmail.com>
2025-08-31 18:36:09 +0300

    corrections-1

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,12 +1,12 @@
-// interface Product {
-//     readonly id: number;
-//     title: string;
-//     description?: string;
-//  }
-// const product:Product = {
-//   id: 1,
-//   title: "Tablet",
-//   description: "Compact and fast",
-// };
+interface Product {
+    readonly id: number;
+    title: string;
+    description?: string;
+}
+const product: Product = {
+    id: 1,
+    title: "Tablet",
+    description: "Compact and fast",
+};
 
-// console.log(`Product: ${JSON.stringify(product)}`);
+console.log(`Product: ${JSON.stringify(product)}`);
--- src/task-8.ts
+++ src/task-8.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
-interface Post{
-    id: number;
-    title: string;
-    body: string;
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
 }
-async function fetchPosts()//:Promise<Post[]>
-{
-  const response = await axios.get<Post[]>(//Array<Post>
-    '<https://jsonplaceholder.typicode.com/posts>'
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
   );
   return response.data;
 }
